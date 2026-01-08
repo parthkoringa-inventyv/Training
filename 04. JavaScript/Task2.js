098742d162791e35d0d4467e3e60b52abd27ad48
Parth <parth.koringa.inventyv@gmail.com>
2026-01-08 14:15:09 +0530

    Task2 completed -PK

---
+++ 04. JavaScript/Task2.js
@@ -0,0 +1,17 @@
+let n = 5;
+let i,j;
+for(i=1 ; i<=n ; i++)
+{
+    for(j=1 ; j<=n ; j++)
+    {
+        process.stdout.write(( (i < j) ? 
+                                 ((i <= n-j) ? 
+                                     ((i <= n-i) ? i : n-i+1) 
+                                     : ((n-j <= n-i) ? n-j+1 : n-i+1)) 
+                                 : 
+                                 ((j <= n-j) ? 
+                                     ((j <= n-i) ? j : n-i+1) 
+                                     : ((n-j <= n-i) ? n-j+1 : n-i+1)) ) + ' ');
+    }   
+    process.stdout.write("\n");
+}
